e3dc2d8a28f702042440e33d438247a3f0ab8f02
Carlos Ayala <cayalav17@gmail.com>
2021-09-04 23:59:41 +0000

    Change("Num1")

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# num 1
+# Num 1
 def num1fun():
     y = ((5+2-5)**2*5+8/2-30)/2*5-3
     return y
@@ -16,3 +16,7 @@ def num3fun():
     m = z**2*3+n;
     y = ((((z+2-n)**2*m+8/2-30)/2*5-3)**5+15*3-9/3)**2-5/4
     return y
+
+# Num 2
+def num1(p, v, t):
+    return (p * v) / (0.37 * (t + 460))
